7f666c27343dd87699f12cfd9258cbd0f2cc4ac4
ydzheng13 <52845683+ydzheng13@users.noreply.github.com>
2021-04-06 15:04:19 -0500

    Test

--- Startup.cs
+++ Startup.cs
@@ -15,7 +15,7 @@ namespace DockerWebAppCICDDemo
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
